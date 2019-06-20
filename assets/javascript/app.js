838107a6c15c14df0df1c4f8d32f486de4bb06af
Andy Ren <andyr914@gmail.com>
2019-06-19 21:41:53 -0700

    charity API officially working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,40 @@
-$("#searchButton").on("click", function(){
+$("#searchButton").on("click", function () {
     charityPull();
     populatePage();
     // console.log("search for this:" + charityPull);
 });
 
-function charityPull(){
+function charityPull() {
     console.log("got into charityPull");
     var searchTerm = $("#search-term").val();
     console.log(searchTerm);
-    var key = "b1bcb5038ce8b872aaf3c3f6c7d3b3eb";
-    $("body").empty();
-    var queryURL = "http://data.orghunter.com/v1/charitysearch?user_key=" + key + "&searchTerm=" + searchTerm;
+    var key = "811f3796206861ae75e263c9f204ca17";
+    var APP_ID = "caa9091c";
+    var queryURL = "https://api.data.charitynavigator.org/v2/Organizations?app_id=" + APP_ID + "&app_key=" + key + "&search=" + searchTerm;
     console.log(queryURL);
     $.ajax({
-        headers: {"access-control-allow-origin": "https://orghunter.3scale.net", "access-control-allow-credentials": "true", "content-type" : "application/json"},
         url: queryURL,
-        method: "POST",
-        dataType: "jsonp",
-        crossDomain: true,
-    }).then(function(response){
-        var result = response.data;
-        console.log("got into then");   
-        console.log(response);
-    }).fail(function(message) {
-        console.log("failed!");
-        console.log(message.status);
-    });
-    
+        method: "GET",
+        dataType: "json",
+    }).then(function (response) {
+        for (var i = 0; i < 10; i++) {
+            var result = response[i].charityName;
+            console.log("got into then");
+            result = "<tr>"
+            console.log(result);
+        }
+    })
+        .fail(function (message) {
+            console.log("failed!");
+            console.log(message);
+        });
+
 };
 
-// function setHeader(xhr){
-//     xhr.setRequestHeader("Authorization", "https://data.orghunter.com/");
-// }
-
-function populatePage(){
-    newPage = "<div class='jumbotron'><h1 class='text-center'>Find That Charity!!!</h1></div>" +
-    "<div class='form-group'><input type='text' class='form-control' id='search-term'>" +
-    "<button type='submit' class='btn btn-default' id='searchButton'>Search</button>" +
-    "</div>"
-    $("body").html(newPage);
+function populatePage() {
+    $("#contentInformation").empty();
+    newPage = "<div class='card'><div class='card-header'>Charities</div>" +
+        "<div class='card-body'><table class='table' id='contentInformation'>" +
+        "<tr><th scope='col'>Charity Name</th></tr></table></div></div>";
+    $("#contentInformation").html(newPage);
 }
