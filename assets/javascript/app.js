5c78c3ba5b63c28f6287fa0bb081471e0a8c249c
Andy Ren <andyr914@gmail.com>
2019-06-19 19:19:13 -0700

    2nd commit 'testing'

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,17 +6,25 @@ $("#searchButton").on("click", function(){
 
 function charityPull(){
     var searchTerm = $("#search-term").val();
+    var key = ["b1bcb5038ce8b872aaf3c3f6c7d3b3eb"];
     $("body").empty();
-    var queryURL = "https://data.orghunter.com/v1/charitysearch?user_key=b1bcb5038ce8b872aaf3c3f6c7d3b3eb&searchTerm=" + searchTerm;
+    var queryURL = "https://data.orghunter.com/v1/charitysearch?user_key=" + key + "&searchTerm=" + searchTerm;
     $.ajax({
         url: queryURL,
-        method: "GET"
+        method: "GET",
+        dataType: "jsonp",
+        crossDomain: true,
+        beforeSend: setHeader
     }).then(function(response){
         var result = response.data;
         console.log(result);
     });
     
 };
+
+function setHeader(xhr){
+    xhr.setRequestHeader("Authorization", token);
+}
 
 function populatePage(){
     newPage = "<div class='jumbotron'><h1 class='text-center'>Find That Charity!!!</h1></div>" +
